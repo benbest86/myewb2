d5a68992b246b31a3443a806880bd73942141679
Joshua Gorner <joshuagorner@ewb.ca>
2009-06-19 20:49:53 +0000

    Changed database to MySQL.
    git-svn-id: https://cotonou.ewb.ca/svn/myewb2/trunk@14 145a74ad-6e29-4586-b527-8b2e899353c9

--- myewb/settings.py
+++ myewb/settings.py
@@ -22,10 +22,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'dev.db'       # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'myewb_development'       # Or path to database file if using sqlite3.
+DATABASE_USER = 'myewbuser'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'd0rothy'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
