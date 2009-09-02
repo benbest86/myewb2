a28207fa41a3d5a6d00dddbabd290d2fd979a8ab
Francis Kung <franciskung@ewb.ca>
2009-09-02 04:52:41 +0000

    oops, missed a file in the merge
    git-svn-id: https://cotonou.ewb.ca/svn/myewb2/trunk@94 145a74ad-6e29-4586-b527-8b2e899353c9

--- myewb/siteutils/helpers.py
+++ myewb/siteutils/helpers.py
@@ -1,5 +1,51 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseForbidden, Http404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from emailconfirmation.models import EmailAddress
+
+def get_obj(app_label=None, model_name=None, ct=None, id=None, slug=None):
+    """ 
+    An error-handled way to get a generic object.
+    You need to specify app_label and model_name, OR the ContentType object (ct)
+      (if you supply a ct, the app_label and model_name will be ignored)
+    And you also have to specify id OR slug
+      (if you supply both, the id will be used)
+    """
+    try:
+        if ct == None:
+            ct = ContentType.objects.get(app_label = app_label,
+                                         model = model_name)
+        if id:
+            obj = ct.get_object_for_this_type( pk=id )
+        else:
+            obj = ct.get_object_for_this_type( slug=slug )
+
+    except:
+        raise Http404('No object found. applabel %s | model %s | id %s' %
+                      (app_label, model_name, id) )
+    
+    return obj
+
+def is_visible(user, obj):
+    """
+    Checks the is_visible method of a generic obj, returning True if the obj
+    doesn't have an is_visible method
+    """
+
+    try:
+        method = getattr(obj, "is_visible")
+        if not method(user):
+            return False
+        
+    except:
+        # the parent object didn't have an is_visible method defined -
+        # assume global visibility (???)
+        # for now shouldn't happen, as event.parent can only be a baes_group
+        pass
+    
+    return True
 
 def get_email_user(email):
     email_user = None
@@ -12,3 +58,4 @@ def get_email_user(email):
             email_user = users_with_the_email[0]
             
     return email_user
+
