93d064a7f44e48ef2974b389f323a76f12c65e83
Ben Best <ben.best@gmail.com>
2009-12-06 21:24:52 -0500

    using extras.create_bulk_user instead of setting is_active=False manually

--- myewb/apps/networks/views/__init__.py
+++ myewb/apps/networks/views/__init__.py
@@ -127,9 +127,7 @@ def bulk_import(request, group_slug, form_class=NetworkBulkImportForm, template_
                     username = User.objects.make_random_password()     # not a password per se, just a random string
                     while User.objects.filter(username=username).count() > 0:   # ensure uniqueness
                         username = User.objects.make_random_password()
-                    email_user = User.objects.create_user(username, email)      # sets "unusable" password
-                    email_user.is_active = False # is_active is the flag for whether a user is a bulk user or not
-                    email_user.save()
+                    email_user = User.extras.create_bulk_user(username, email)      # sets "unusable" password
                 
                 existing_members = GroupMember.objects.filter(group=group, user=email_user)
                 if existing_members.count() == 0:              
