a675dd17e09029eb9290240e7259e6f35dc16bfd
Francis Kung <franciskung@ewb.ca>
2009-12-11 01:48:28 -0500

    Update git revision display to work in deployment environment

--- myewb/siteutils/templatetags/git_revision.py
+++ myewb/siteutils/templatetags/git_revision.py
@@ -7,6 +7,7 @@ from django import template
 #from django.utils.version import get_svn_revision
 #from django.db.models.loading import get_app
 import subprocess
+import settings
 
 register = template.Library()
 
@@ -21,12 +22,14 @@ def git_date():
     
     if gdate == None:
         _proc = subprocess.Popen('git log --no-color -n 1',
-                                 shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                 shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 cwd=settings.PROJECT_ROOT)
         try:
             gdate = [x.split('Date:')[1].strip() for x in
                      _proc.communicate()[0].splitlines() if x.startswith('Date:')][0]
             datearray = gdate.split(' ')
             gdate = "%s %s, %s" % (datearray[1], datearray[2], datearray[4])
+
         except IndexError:
             gdate = 'unknown'
         
@@ -37,12 +40,14 @@ def git_rev():
     global grev
     
     if grev == None:
-        _proc = subprocess.Popen('git log --no-color -n 1 --date=iso',
-                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _proc = subprocess.Popen('git log --no-color -n 1',
+                                 shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                 cwd=settings.PROJECT_ROOT)
         try:
             grev = [x.split('commit')[1].strip() for x in
                     _proc.communicate()[0].splitlines() if x.startswith('commit')][0]
         except IndexError:
             grev = 'unknown'
-        
+
     return grev
+
