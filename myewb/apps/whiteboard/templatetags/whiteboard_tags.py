025373f9ffb39c5eb1152c7cbe3d5b47dc26e08c
Francis Kung <francis@fkung-lpt.localdomain>
2010-01-01 18:53:22 -0500

    More networks template work:
    - style group subsections nicely
    - always display the whiteboard
    - style posts properly
    - put networks toolbars in networks/base.html instead of network_detail.html

--- myewb/apps/whiteboard/templatetags/whiteboard_tags.py
+++ myewb/apps/whiteboard/templatetags/whiteboard_tags.py
@@ -5,8 +5,25 @@ register = Library()
 
 @register.inclusion_tag("whiteboard/whiteboard.html")
 def show_whiteboard(whiteboard, group, member):
+    """
+    Show a whiteboard.  If the whiteboard is empty, show a link to create one.
+    """
     return {
         "wb": whiteboard,
         "group": group,
         "member": member,
+        "force": False
     }
+
+@register.inclusion_tag("whiteboard/whiteboard.html")
+def show_whiteboard_force(whiteboard, group, member):
+    """
+    Show a whiteboard, even if it's empty.
+    """
+    return {
+        "wb": whiteboard,
+        "group": group,
+        "member": member,
+        "force": True
+    }
+
