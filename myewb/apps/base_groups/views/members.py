d076647844fe2bc9084db922e42f6958d7995bed
Francis Kung <franciskung@ewb.ca>
2010-01-27 16:29:32 -0500

    Fix bug in group member mgmt

--- myewb/apps/base_groups/views/members.py
+++ myewb/apps/base_groups/views/members.py
@@ -192,6 +192,7 @@ def edit_member(request, group_slug, username, group_model=None, form_class=None
     # grab basic objects
     group = get_object_or_404(group_model, slug=group_slug)
     other_user = get_object_or_404(User, username=username)
+    member = get_object_or_404(GroupMember, group=group, user=other_user)
     user = request.user
     
     # saving the object
@@ -205,7 +206,6 @@ def edit_member(request, group_slug, username, group_model=None, form_class=None
 
     # just load up the info and put it in a new form
     else:
-        member = get_object_or_404(GroupMember, group=group, user=other_user)
         form = form_class(instance=member)
         
     return render_to_response(template_name,
