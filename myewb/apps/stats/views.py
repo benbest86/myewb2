5bff3f5a09ec436a9f91fb020a922dc21270704b
Francis Kung <franciskung@ewb.ca>
2010-04-05 06:16:35 -0400

    First stats graph (#216)

--- myewb/apps/stats/views.py
+++ myewb/apps/stats/views.py
@@ -6,11 +6,17 @@ Copyright 2010 Engineers Without Borders (Canada) Organisation and/or volunteer 
 @author Francis Kung
 """
 
-from datetime import date
+from datetime import date, timedelta
 
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+
+from pygooglechart import Chart
+from pygooglechart import SimpleLineChart
+from pygooglechart import XYLineChart
+from pygooglechart import SparkLineChart
+from pygooglechart import Axis
 
 from stats.models import DailyStats
 
@@ -18,6 +24,45 @@ from stats.models import DailyStats
 def main_dashboard(request):
     
     today, created = DailyStats.objects.get_or_create(day=date.today())
+    
+    #twoweeks = date.today() - timedelta(days=14)
+    #averageusage = DailyStats.objects.filter(day__gt=twoweeks)
+    averageusage = DailyStats.objects.all()
+    max_signins = 0
+    days = []
+    signins = []
+    posts = []
+    replies = []
+    whiteboards = []
+    for s in averageusage:
+        if s.signins > max_signins:             # hack until django 1.1 and aggregate queries
+            max_signins = s.signins
+        days.append(s.day.strftime("%B %y"))
+        signins.append(s.signins)
+        posts.append(s.posts)
+        replies.append(s.replies)
+        whiteboards.append(s.whiteboardEdits)
+        
+    chart = SimpleLineChart(600, 450, y_range=(0, max_signins))
+    #chart.add_data(avgsignins)
+    chart.add_data(posts)
+    chart.add_data(replies)
+    chart.add_data(signins)
+    chart.add_data(whiteboards)
+
+    chart.set_colours(['ff0000', 'ffff00', '00ff00', '0000ff'])
+    chart.set_legend(['posts', 'replies', 'signins', 'whiteboards'])
+    chart.set_legend_position('b')
+
+    xaxis = []
+    for i in range(0, len(days), len(days)/1):    # that last number should be 8
+        xaxis.append(days[i])
+    yaxis = range(0, max_signins + 1, 2)    # that last number should be 25 or 50.  but for testing...
+    yaxis[0] = ''
+    chart.set_axis_labels(Axis.LEFT, yaxis)
+    chart.set_axis_labels(Axis.BOTTOM, days)
+        
+    dailyUsage = chart.get_url()
     
     return render_to_response("stats/dashboard.html",
                               {"signins": today.signins,
@@ -30,7 +75,7 @@ def main_dashboard(request):
                                "regupgrades": today.regupgrades,
                                "regdowngrades": today.regdowngrades,
                                "renewals": today.renewals,
-                               "testurl": url
+                               "dailyUsage": dailyUsage
                               },
                               context_instance=RequestContext(request))
     
