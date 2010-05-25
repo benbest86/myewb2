758d15fa47e6705e53bf23ecd6f80f62f08a82d1
Sean Boots <sean.boots@gmail.com>
2010-05-25 13:26:30 -0400

    Don't crash if there are no posts in the system

--- myewb/siteutils/context_processors.py
+++ myewb/siteutils/context_processors.py
@@ -3,7 +3,11 @@ from group_topics.models import GroupTopic
 
 def myewb_settings(request):
     gt = GroupTopic.objects.all().order_by('-last_reply')[:1]
-    
+    try:
+        latest_post = gt[0].last_reply
+    except:
+        latest_post = 0
+
     grandfathered = False
     if request.user.is_authenticated():
         grandfathered = request.user.get_profile().grandfathered
@@ -12,6 +16,6 @@ def myewb_settings(request):
         request.user.get_profile().save()
     
     return {'CACHE_TIMEOUT': settings.CACHE_TIMEOUT,
-            'LATEST_POST': gt[0].last_reply,
+            'LATEST_POST': latest_post,
             'grandfathered': grandfathered}
     
