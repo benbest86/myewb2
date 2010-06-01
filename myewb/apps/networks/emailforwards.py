872d09978e465a5d9946c3216c1079f0afebdac9
Francis Kung <franciskung@ewb.ca>
2010-06-01 17:30:51 -0400

    More email forwards stuff

--- myewb/apps/networks/emailforwards.py
+++ myewb/apps/networks/emailforwards.py
@@ -9,6 +9,7 @@ Last modified on 2009-08-17
 """
 
 from django import forms
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 from emailconfirmation.models import EmailAddress
