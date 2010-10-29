501809e6132286855c00ebcf39334319419c8980
Ben Best <ben.best@gmail.com>
2010-10-29 00:10:01 +0000

    first stab at cohorts and invitations models.

--- myewb/apps/confcomm/models.py
+++ myewb/apps/confcomm/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from profiles.models import MemberProfile
+from networks.models import Network
 from avatar.templatetags.avatar_tags import avatar_url
 
 # Create your models here.
@@ -40,3 +41,53 @@ class ConferenceProfile(models.Model):
     def username(self):
         return self.member_profile.user.username
 
+CHAPTER_CHOICES = []
+chapterlist = Network.objects.filter(chapter_info__isnull=False, is_active=True).order_by('name')
+for chapter in chapterlist:
+    CHAPTER_CHOICES.append((chapter.slug, chapter.chapter_info.chapter_name))
+
+CANADA_ROLE_CHOICES = (
+        ('e', 'Executive',),
+        ('p', 'President',),
+        ('m', 'Member',),
+        )
+class CanadaCohort(models.Model):
+    """
+    A definition of a Cohort of people that we are looking at.
+    """
+    chapter = models.CharField(max_length=2,choices=CHAPTER_CHOICES)
+    role = models.CharField(max_length=1, choices=CANADA_ROLE_CHOICES)
+    year = models.PositiveIntegerField()
+    members = models.ManyToManyField(MemberProfile)
+
+AFRICA_ROLE_CHOICES = (
+        ('j', 'Junior Fellow/Operation 21',),
+        ('s', 'African Programs Staff/Overseas Volunteer Staff',),
+        ('p', 'Professional Fellow',),
+        )
+AFRICA_COUNTRY_CHOICES = (
+        ('b', 'Burkina Faso',),
+        ('g', 'Ghana',),
+        ('m', 'Malawi',),
+        ('z', 'Zambia',),
+        ('p', 'Philippines',),
+        ('t', 'Tanzania',),
+        ('c', 'Cameroon',),
+        )
+class AfricaCohort(models.Model):
+    """
+    A definition of a Cohort in terms of Africa.
+    """
+    country = models.CharField(_('Country'), max_length=1, choices=ARFICA_COUNTRY_CHOICES) # Ghana, Burkina, etc.
+    role = models.CharField(_('Role'), max_length=1, choices=AFRICA_ROLE_CHOICES) # APS/OVS, JF/OP21, ProF
+    year = models.PositiveIntegerField()
+    members = models.ManyToManyField(MemberProfile)
+
+class ConferenceInvitation(models.Model):
+    """
+    Keep track of invitations that are sent and clicked on.
+    """
+    sender = models.ForeignKey(MemberProfile)
+    receiver = models.ForeignKey(MemberProfile)
+    code = models.CharField()
+    activated = models.BooleanField()
