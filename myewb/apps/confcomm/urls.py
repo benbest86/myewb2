1b1b1ebe6d17cd1e6f4ba4f9ae53cb124b47b972
Ben Best <ben.best@gmail.com>
2010-11-02 14:35:31 +0000

    modifying urls to allow for usernames with dots at the beginning.

--- myewb/apps/confcomm/urls.py
+++ myewb/apps/confcomm/urls.py
@@ -20,16 +20,16 @@ urlpatterns = patterns('confcomm.views',
 
         url('^profile/$', 'profile', name='confcomm_profile'),
         url('^profile/edit/$', 'profile_edit', name='confcomm_profile_edit'),
-        url('^profile/(?P<username>\w+)/$', 'profile', name='confcomm_profile'),
+        url('^profile/(?P<username>\.?\w+)/$', 'profile', name='confcomm_profile'),
 
         # ajaxified
         url('^send_invitation/$', 'send_invitation', name='confcomm_email'),
         url('^update_avatar/$', 'update_avatar', name='confcomm_avatar'),
-        url('^piston/profile/(?P<username>\w+)/$', confprof_handler, {'emitter_format': 'json'}, name="confcomm_profile_api"),
+        url('^piston/profile/(?P<username>\.?\w+)/$', confprof_handler, {'emitter_format': 'json'}, name="confcomm_profile_api"),
         url('^piston/profile/$', confprof_handler, {'emitter_format': 'json'}, name="confcomm_profile_api_base"),
         url('^piston/profiles/$', confprof_handler, {'emitter_format': 'json'}, name="confcomm_profile_api_profiles"),
         url('^piston/cohorts/$', cohort_handler, {'emitter_format': 'json'}, name='confcomm_cohorts'),
         url('^piston/cohort_members/$', cohort_member_handler, {'emitter_format': 'json'}, name='confcomm_cohort_members'),
         url('^piston/cohort_members/(?P<id>\d+)/$', cohort_member_handler, {'emitter_format': 'json'}, name='confcomm_cohort_members'),
-        url('^piston/cohort_members/(?P<id>\d+)/(?P<username>\w+)/$', cohort_member_handler, {'emitter_format': 'json'}, name='confcomm_cohort_members'),
+        url('^piston/cohort_members/(?P<id>\d+)/(?P<username>\.?\w+)/$', cohort_member_handler, {'emitter_format': 'json'}, name='confcomm_cohort_members'),
 )
