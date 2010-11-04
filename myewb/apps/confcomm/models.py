eb0227a8da6355404f8813649ab60f71f254109c
Ben Best <ben.best@gmail.com>
2010-11-04 11:44:03 +0000

    Updating copy re: andrew.

--- myewb/apps/confcomm/models.py
+++ myewb/apps/confcomm/models.py
@@ -32,11 +32,11 @@ class ConferenceProfile(models.Model):
     member_profile = models.OneToOneField(MemberProfile, unique=True, verbose_name=_('member profile this profile is linked to.'))
     registered = models.BooleanField(_('registered for conference 2011'), default=False)
     # additional personal information
-    what_now = models.TextField(_("What you are doing now."), default="Edit me! What are you doing? Where are you living?")
+    what_now = models.TextField(_("What you are doing now."),)
     # additional information we want for conference.
     interests = models.ManyToManyField(ConferenceInterest, related_name='interested_users', verbose_name=_('List of interests.'), blank=True)
-    conference_question = models.TextField(_("One question you want to answer at conference."), default="Add your question here!")
-    conference_goals = models.TextField(_("Your goals for conference."), default="My goals for conference are...")
+    conference_question = models.TextField(_("One question you want to answer at conference."),);
+    conference_goals = models.TextField(_("Your goals for conference."),)
     active = models.BooleanField(default=False)
 
     @property
