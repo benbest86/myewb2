e48dc9f09ac174a94d1eb3918afdb89758598451
Alex Joyce <alexjoyce@ewb.ca>
2010-12-08 23:28:05 -0500

    exclude cm from dashboard, improved admin, include NO transactions on dashboard monthly, fix totals on dashboards, tried NO over-time graphs

--- myewb/apps/finance/admin.py
+++ myewb/apps/finance/admin.py
@@ -2,12 +2,26 @@ from django.contrib import admin
 from finance.models import Transaction
 from finance.models import  MonthlyReport
 from finance.models import Income
-from finance.models import Expenditure, Donation, Category, Budget, BudgetItem
+from finance.models import Expenditure
+from finance.models import Donation
+from finance.models import Category
+from finance.models import Budget
+from finance.models import BudgetItem
 #, Event
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('bank_date', 'account', 'type', 'amount', 'group', 'category', 'enter_date', 'monthlyreport')
+    list_filter = ['group']
+    search_fields = ['description']
+    date_hierarchy = 'bank_date'
 
-admin.site.register(MonthlyReport)
-admin.site.register(Transaction)
+class MonthlyReportAdmin(admin.ModelAdmin):
+    list_display = ('date', 'type', 'enter_date', 'group')
+    list_filter = ['group']
+    
+
+admin.site.register(MonthlyReport, MonthlyReportAdmin)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Income)
 admin.site.register(Expenditure)
 admin.site.register(Donation)
