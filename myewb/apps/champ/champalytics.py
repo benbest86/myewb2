b8c5bccbb0de507f8d0d9fc7d19d84e2705ec4ba
franciskung <franciskung@ewb.ca>
2010-12-28 01:53:07 -0500

    More work toward progress graphs

--- myewb/apps/champ/champalytics.py
+++ myewb/apps/champ/champalytics.py
@@ -21,7 +21,7 @@ from base_groups.decorators import group_admin_required
 from networks.decorators import chapter_president_required, chapter_exec_required
 from networks.models import Network
 from champ.models import *
-from champ.forms import *
+from champ.views import *
 from siteutils import schoolyear
 from siteutils.helpers import fix_encoding, copy_model_instance
 from siteutils.http import JsonResponse
@@ -29,16 +29,18 @@ from siteutils.http import JsonResponse
 def default(request):
     if not request.GET.get('stage', None):
         return select_graph(request)
-    elif request.GET['stage'] == 'options':
+    elif request.GET['stage'] == 'options' or request.GET['stage'] == 'draw':
+        stage = request.GET['stage']
         graphtype = request.GET.get('graphtype', None)
 
         if graphtype == 'Progress to goal':
-            return options_progress(request)
+            if stage == 'options':
+                return progress_options(request)
+            elif stage == 'draw':
+                return progress_draw(request)
+                
         
-    elif request.GET['stage'] == 'draw':
-        return draw_graph(request)
-
-
+        
     return HttpResponse("Internal error - lost stage")
         
 def select_graph(request):
@@ -46,18 +48,75 @@ def select_graph(request):
     group = request.GET.get('group', '')
     
     return render_to_response('champ/champalytics/select.html',
-                              {metric: metric,
-                               group: group},
+                              {'metric': metric,
+                               'group': group},
                               context_instance=RequestContext(request))
+                              
+def build_stats(group_slug=None):
+    activity_filters, metric_filters = build_filters(date.today().year)
+    
+    if group_slug:
+        grp = get_object_or_404(Network, slug=group_slug)
+        
+        if grp.is_chapter():
+            activity_filters.append({'group__slug': group_slug})
+            metric_filters.append({'activity__group__slug': group_slug})
+        else:
+            grp = None
 
-def options_progress(request):
+    activity_filters.append({'visible': True})
+    metric_filters.append({'activity__visible': True})
+    metric_filters.append({'activity__confirmed': True})
+    context = run_stats(metric_filters)
+
+    context['unconfirmed'] = run_query(Activity.objects.filter(confirmed=False), activity_filters).count()
+    context['confirmed'] = run_query(Activity.objects.filter(confirmed=True), activity_filters).count()
+    
+    natl_activity_filters, natl_metric_filters = build_filters(date.today().year)
+    natl_activity_filters.append({'visible': True})
+    natl_metric_filters.append({'activity__visible': True})
+    natl_metric_filters.append({'activity__confirmed': True})
+    natl_context = run_stats(natl_metric_filters)
+    natl_context['unconfirmed'] = run_query(Activity.objects.filter(confirmed=False), natl_activity_filters).count()
+    natl_context['confirmed'] = run_query(Activity.objects.filter(confirmed=True), natl_activity_filters).count()
+
+    # stuff all national values into context, prepending key with natl_
+    for x, y in natl_context.items():
+        context['natl_' + x] = y
+
+    return context
+
+def progress_options(request):
     metric = request.GET.get('metric', '')
     group = request.GET.get('group', '')
     
-    return render_to_response('champ/champalytics/options_progress.html',
-                              {metric: metric,
-                               group: group},
+    return render_to_response('champ/champalytics/progress_options.html',
+                              {'metric': metric,
+                               'group': group},
                               context_instance=RequestContext(request))
+                              
+def progress_draw(request):
+    metric = request.GET.get('metric', '')
+    group = request.GET.get('group', '')
+    progressby = request.GET.get('progressby', '')
+    context = {}
+    
+    if progressby == 'formetric' and metric:
+        stats = build_stats()
+        template = 'progress_metric'
+        
+    
+    elif progressby == 'forchapter' and group:
+        stats = build_stats(group)
+        template = 'progress_chapter'
+    
+    else:
+        return HttpResponse('oops')
+    
+    return render_to_response('champ/champalytics/' + template + '.html',
+                              context,
+                              context_instance=RequestContext(request))
+                              
 
 def draw_graph(request):
     return HttpResponse("graph")
