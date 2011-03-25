7700cca8b37b9aebe1cd2f1020fb2ee59d192e4d
Francis Kung <franciskung@ewb.ca>
2011-03-25 15:27:34 -0400

    Updates to job board models

--- myewb/apps/jobboard/models.py
+++ myewb/apps/jobboard/models.py
@@ -24,12 +24,21 @@ class JobPosting(models.Model):
     owner = models.ForeignKey(User)
     
     posted_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
     deadline = models.DateField(blank=True, null=True)
 
     urgency = models.CharField(max_length=10, choices=URGENCY_CHOICES)
-    #skills = models.ManyToManyField(Skill)
+    skills = models.ManyToManyField('jobboard.Skill')
     time_required = models.CharField(max_length=10, choices=TIME_CHOICES)
     
+    active = models.BooleanField(default=True)
+    
+    accepted_users = models.ManyToManyField(User, related_name='accepted_jobs')
+    bid_users = models.ManyToManyField(User, related_name='bid_jobs')
+    following_users = models.ManyToManyField(User, related_name='following_jobs')
     
     objects = JobPostingManager()
 
+class Skill(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    
