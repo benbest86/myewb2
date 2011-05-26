690b921ef97b90f07bc42b68a5bdb9ff80ad902e
Alex Joyce <alexjoyce@ewb.ca>
2011-05-26 13:23:24 -0700

    delete files that were a mistake upload from December

--- transactions/admin.py
+++
@@ -1,7 +0,0 @@
-from transactions.models import IncomeCategory, ExpenditureCategory, Income, Expenditure
-from django.contrib import admin
-
-admin.site.register(Income)
-admin.site.register(Expenditure)
-admin.site.register(IncomeCategory)
-admin.site.register(ExpenditureCategory)
